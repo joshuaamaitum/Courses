984e19615523496fbd378f68c28f052d69739b58
joshuaamaitum <130652730+joshuaamaitum@users.noreply.github.com>
2025-04-11 21:48:44 +0200

    1

--- Tutorial_R/R_FRA_script.R
+++ Tutorial_R/R_FRA_script.R
@@ -34,6 +34,7 @@ names(data) # Names of columns
 rownames(data) # Names of rows
 head(data) # First 6 rows
 tail(data) # Last 6 rows
+head(titles) # First 6 rows
 
 #=============================================================
 # 4.2.2 - Types of variables
@@ -47,6 +48,26 @@ is.numeric(data) # Checks if variable is numeric
 which(data) # Returns list of elements having a true value
 Vn = which(sapply(data, is.numeric)) # Determine the position of the numeric variables
 Vn
+
+#=============================================================
+# 4.3 - Selecting subsets of data
+#=============================================================
+
+#=============================================================
+# 4.3.1 - Selection with numbers
+#=============================================================
+
+head(data) # First 6 rows of data
+data[3,] # Extract a single row, e.g. 3
+data[c(1:3,6),] # Extract several specific rows
+data[,1] # Extract a single column, e.g. 1
+data[1:5,1:2] # Select ranges of specific rows & columns
+data[c(10:15,30:35),1:2] # Select ranges of specific rows & columns
+data[10:20,-3] # Selection by removing specific row
+data[50:60,-c(2,3)] # Selection by removing several rows
+data[-c(1:3,6:230),] # Selection by removing rows 1-3 and 6-230
+data[-(11:nrow(data)),] # Selection by removing all rows from the 11th to the last
+data[c(71,82,127,130,169),] # Selection of French overseas territories
 
 #=============================================================
 # 4.3.2 - Selection with names
