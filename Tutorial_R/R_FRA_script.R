cd0ff6cfd4f8374240c006fa01bd9d3d8ed679b8
joshuaamaitum <130652730+joshuaamaitum@users.noreply.github.com>
2025-04-11 21:08:56 +0200

    1

--- Tutorial_R/R_FRA_script.R
+++ Tutorial_R/R_FRA_script.R
@@ -12,11 +12,21 @@ rm(list = ls()) #Clean R's memory
 #=============================================================
 
 # Setting the working directory
-setwd("G:/My Drive/GitHub Desktop/Courses/Tutorial_R") # Update
+setwd("G:/My Drive/GitHub Desktop/Courses/Tutorial_R") # Update working directory 
 
 # Reading the first data set and the titles
-Data = read.csv2("FRA_data1.csv",row.names=1) 
-Titles = read.csv2("FRA_titles.csv",row.names=1)
+data = read.csv2("FRA_data1.csv",row.names=1) 
+titles = read.csv2("FRA_titles.csv",row.names=1)
+
+#=============================================================
+# 4.2 - General information on the data structure
+#=============================================================
+
+#=============================================================
+# 4.2.1 - Main functions
+#=============================================================
+
+str
 
 #=============================================================
 # 4.3.2 - Selection with names
