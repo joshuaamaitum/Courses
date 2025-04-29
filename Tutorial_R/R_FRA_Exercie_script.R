89ea6f6410bf26fbf4853ea9cf4504185ad7a1f4
joshuaamaitum <130652730+joshuaamaitum@users.noreply.github.com>
2025-04-29 16:50:06 +0200

    1

--- Tutorial_R/R_FRA_Exercie_script.R
+++ Tutorial_R/R_FRA_Exercie_script.R
@@ -10,6 +10,7 @@ setwd("G:/My Drive/GitHub Desktop/Courses/Tutorial_R")
 
 # Import the file
 data1 = read.csv2("FRA_data2.csv", row.names=1)
+names = read.csv2("FRA_titles.csv",row.names=1)
 
 # 21. For how many territories do we have data
 nrow(data1)
@@ -35,10 +36,65 @@ head(forest_area)
 
 # 28. Which territory has the highest forest area change
 names(data1)
+FAC = data1[order(data1$FAC,decreasing=T),]
+head(FAC)
 
+# 29. Which territory has the lowest forest area change
+FAC = data1[order(data1$FAC),]
+head(FAC)
 
+# 30. The 3 variables having more than 40 missing values
+MV = is.na(data1)
+sum(MV)
+MVr = apply(MV,1,sum)
+MVr
+MVc = apply(MV,2,sum)
+MVc
 
+# 31. How many countries have no missing values
+table(MVr)
 
+# 32. Identify the countries with 5 missing values
+data1[MVr>5,]
 
+# 33. Under which climate are other wooded lands most common
+names(data1)
+owa = aggregate(data1[,6], list(data1$Climate), sum, na.rm=T)
+owa
+
+# 34. Which climate displays two-thirds of the world's biomass
+names(data1)
+bio = aggregate(data1[,15], list(data1$Climate), sum, na.rm=T)
+bio
+
+# 35. Under what climate do we find more than half of the world's forest plantations
+names(data1)
+pla = aggregate(data1[,9],list(data1$Climate), sum, na.rm=T)
+pla
+
+# 36. Under what climate do we find half of the world's primary forests
+names(data1)
+pri = aggregate(data1[,8], list(data1$Climate), sum, na.rm=T)
+pri
+
+# 37. Climate with more than 60% of the world's coniferous forest volume
+names(data1)
+con = aggregate(data1[,12], list(data1$Climate), sum, na.rm=T)
+con
+
+# 38. Climate with forest area decrease of 4 million hectares per year
+names(data1)
+fac = aggregate(data1[,7], list(data1$Climate), sum, na.rm=T)
+fac
+
+# 39. Forests having the lowest species diversity
+names(data1)
+div = aggregate(data1[,14], list(data1$Climate), sum, na.rm=T)
+div
+
+# 40. Climate with lowest forest volume per hectare
+names(data1)
+vol = aggregate(data1[,11], list(data1$Climate), sum, na.rm=T)
+vol
 
 
